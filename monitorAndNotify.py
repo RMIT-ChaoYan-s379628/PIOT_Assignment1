7c278af2a3e4c45d0255158ec4045f6de4d013fa
RMIT-ChaoYan-s379628 <s3579628@student.rmit.edu.au>
2019-03-22 16:50:18 +1100

    make it better

--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -47,13 +47,20 @@ def notify(temp, humi):
     curs.execute(
         "CREATE TABLE IF NOT EXISTS NOTIFICATION_data(timestamp DATETIME, count NUMERIC)")
     curs.execute(
-        "INSERT INTO SENSEHAT_data values(date('now'), (0)")
+        "INSERT INTO NOTIFICATION_data ( timestamp, count ) SELECT * FROM( SELECT date( 'now' ), 0 ) AS tmp WHERE NOT EXISTS (SELECT timestamp FROM NOTIFICATION_data WHERE timestamp = date( 'now' ) ) LIMIT 1; ")
     if (temp>max_tem or temp<min_tem or humi>max_hum or humi<min_hum):
-        curs.execute("select * from NOTIFICATION_data where timestamp=date('now');")
-        for row in curs.fetchone():
-            if row[1]==0 :
-                push = pb.push_note("This is the title", "This is the body")
-        curs.execute("update NOTIFICATION_data set count=1 where timestamp=date('now');")
+        curs.execute(
+            "select count from NOTIFICATION_data where timestamp=date('now');")
+        for row in curs.fetchall():
+            if row[0] == 0:
+                print("ok")
+                pb = Pushbullet('o.p6OF0MXEo2huO3Ajf687fBUczZsTfvHV')
+                pb.push_note("Notification from Raspberry", "The data is out of range.")
+                curs.execute(
+                "update NOTIFICATION_data set count=1 where timestamp=date('now');")
+            else:
+                curs.execute(
+                "update NOTIFICATION_data set count=1 where timestamp=date('now');")
     conn.commit()
     conn.close()
 
