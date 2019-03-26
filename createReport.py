00b09457a7a4a747de86e1f3a12f0c2a295b67d1
RMIT-ChaoYan-s379628 <s3579628@student.rmit.edu.au>
2019-03-26 12:47:09 +1100

    format

--- createReport.py
+++ createReport.py
@@ -15,14 +15,12 @@ with open('report.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     conn = sqlite3.connect('sensehat.db')
     cur = conn.cursor()
-    sql = ("select strftime('%Y-%m-%d',timestamp) as date ,"
-           "AVG(temp) as temp_average, AVG(humi) as humi_average"
-           "from SENSEHAT_data group by strftime('%Y-%m-%d', timestamp);"
-           )
+    sql = '''select strftime('%Y-%m-%d',timestamp) as date ,
+           AVG(temp) as temp_average, AVG(humi) as humi_average
+           from SENSEHAT_data group by strftime('%Y-%m-%d', timestamp);'''
     cur.execute(sql)
     writer.writeheader()
     for row in cur.fetchall():
-        print(row[1])
         if row[1] <= max_tem and row[1] >= min_tem:
             if row[2] <= max_hum and row[2] >= min_hum:
                 writer.writerow({'Data': row[0], 'Status': "OK"})
