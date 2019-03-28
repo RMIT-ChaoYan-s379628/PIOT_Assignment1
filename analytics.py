8cf2a0830e266d940aca9f9f34e4d22d1cfb31ad
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2019-03-28 21:40:01 +1100

    Add files via upload
    Add analytics of data visualisation with temperature and humidity two graph.

---
+++ analytics.py
@@ -0,0 +1,127 @@
+import sqlite3
+from pushbullet import Pushbullet
+from pyecharts import Bar
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
+import pandas as pd
+from dateutil.parser import parse
+import pyqtgraph as pg
+import pyqtgraph.exporters
+import numpy as np
+import pygal
+#import cairosvg
+import os
+from IPython.display import SVG
+from datetime import datetime, timedelta
+from IPython import display
+x_ticks = []
+y_temp = []
+x = []
+def check(count):
+    conn = sqlite3.connect('sensehat.db')
+    curs = conn.cursor()
+
+
+    y=[]
+
+
+#    curs.execute(
+#       "select * from NOTIFICATION_data ;")
+    curs.execute(
+        "select count(1) from SENSEHAT_data; ")
+    count_num = curs.fetchone()[0]
+    print(count_num)
+    curs.execute(
+        "select timestamp,humi,temp from SENSEHAT_data; ")
+
+    for index,row in enumerate(curs.fetchall()):
+        #print(row)
+        if index+1 == count_num/15:
+
+            break
+        x .append(row[0])
+        y .append(row[1])
+        y_temp.append(row[2])
+        if index %5==0:
+            x_ticks.append(row[0])
+
+
+    print(x_ticks)
+    print("画出湿度")
+    #fig1 = plt.figure()
+    fig1 = plt.figure(figsize=(20, 10))
+    ax1 = fig1.add_subplot(1, 1, 1)
+    ax1.xaxis.set_major_formatter(mdate.DateFormatter('%Y-%m-%d %H:%M:%S'))  # 设置时间标签显示格式
+    print(x[0])
+    a = parse(x[0])
+    b = parse(x[len(x)-1])
+    the_diff = (a-b).total_seconds()
+    init_div = 180
+    #计算差异值
+    while True:
+        if the_diff % init_div==0:
+            break
+        else:
+            init_div = init_div-1
+
+    print(len(x))
+
+    print(len(y))
+    print(pd.date_range(start=x[0], end=x[len(x)-1],freq=str(init_div)+'s',normalize=False))
+    print(x)
+    print(y)
+    #dd=pd.date_range(start=x[0], end=x[len(x)-1],freq=str(init_div)+'s',normalize=False)
+    #plt.xticks(pd.date_range(start=x[0], end=x[len(x)-1],freq='s',normalize=False), rotation=90)
+    ax1.set_xticks(range(len(x)))
+    ax1.set_xticklabels(x, rotation=90)
+    plt.title("The Humidity Graph",fontsize=20)
+    plt.xlabel("DateTime",fontsize=20)
+    plt.ylabel("Humidity(%)",fontsize=20)
+    plt.plot(x, y)
+    plt.savefig('Humidity.jpg')
+    #plt.show()
+    print("湿度图完成")
+    date_chart = pygal.Line(x_label_rotation=90)
+    #date_chart.x_labels = map(lambda d: d.strftime('%Y-%m-%d '), x_ticks)
+    date_chart.title = "The Temperature Graph"
+    date_chart.x_labels =x
+    date_chart.add("Temperature(℃)", y_temp)
+    date_chart.render_to_file('bar_chart.svg')
+    #date_chart.render_to_png(filename='bar_chart.png')
+    #fileHandle = open('bar_chart.svg')
+    #svg = fileHandle.read()
+    #fileHandle.close()
+    #exportPath = os.path.join(os.path.abspath(__file__)+'\\', 'bar_chart.png')  # 生成目标文件路径
+    #cairosvg.svg2png(bytestring=svg, write_to=exportPath)  # 转换为png文件
+
+
+
+
+def test_plotscene():
+    pg.setConfigOption('foreground', (255,255,255))
+    y_temp=[1,2,1]
+    x=['2018-03-01 18:01:22','2018-03-01 18:01:23','2018-03-01 18:01:25']
+    w = pg.GraphicsWindow()
+    p = w.addPlot()
+    #ax1.xaxis.set_major_formatter(mdate.DateFormatter('%Y-%m-%d %H:%M:%S'))  # 设置时间标签显示格式
+    p.plot(x,y_temp, pen={'color':'w'})
+    #p.setXRange(0,20)
+    #p.setYRange(-10,20)
+    app = pg.mkQApp()
+    app.processEvents()
+    ex = pyqtgraph.exporters.ImageExporter(w.scene())
+    ex.export(fileName="tem.png")
+def ttt():
+    date_chart = pygal.Line(x_label_rotation=20)
+    date_chart.x_labels = map(lambda d: d.strftime('%Y-%m-%d'), [
+        datetime(2013, 1, 2),
+        datetime(2013, 1, 12),
+        datetime(2013, 2, 2),
+        datetime(2013, 2, 22)])
+    date_chart.add("Visits", [300, 412, 823, 672])
+    date_chart.render_to_file('bar_chart.svg')
+    date_chart.render_to_png(filename='bar_chart.png')
+
+check(10)
+#test_plotscene()
+#ttt()
